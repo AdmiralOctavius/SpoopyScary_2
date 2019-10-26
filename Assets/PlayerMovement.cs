7cf2227f2fefcef05d1aadf5be81585b495712da
Izzy Bennett <buildgamesnotbombs@gmail.com>
2019-10-26 11:56:48 -0400

    Created Project
    Worked with the Brackeys tutorial to add a player controller

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool isJumping = false;
+    bool isCrouching = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //controller.Move();
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            isCrouching = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            isCrouching = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, isCrouching, isJumping);
+        isJumping = false;
+        
+    }
+}
