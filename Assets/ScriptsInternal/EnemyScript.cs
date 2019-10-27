368add3ac0883ce9c126004464c0511f4a8057e5
Izzy Bennett <buildgamesnotbombs@gmail.com>
2019-10-27 18:13:37 -0400

    Got close
    Enemy detection is bugged
    Mothman animation is bugged
    Need to finish forest, office, mines, and chase

---
+++ Assets/ScriptsInternal/EnemyScript.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public bool isStunned = false;
+    public float stunTime;
+
+    public bool facingRight = false;
+
+    LayerMask playerMask;
+
+
+    public GameObject childEyes;
+    public GameObject childEyesRight;
+
+    public CharacterController2D controller;
+
+    public float moveSpeed;
+    public bool attackCooldown;
+
+    public GameObject target1;
+    public GameObject target2;
+    public bool goingRightPath = true;
+
+    public bool chasingPlayer = false;
+
+    RaycastHit2D check;
+    float distance;
+    public Animator animatorZombie;
+
+    bool ifMothMan = false;
+
+    int layerMask = 1 << 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(tag);
+        //playerMask = LayerMask.GetMask("Player");
+        goingRightPath = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (!isStunned)
+        {
+            animatorZombie.SetBool("Walking", true);
+
+            facingRight = controller.m_FacingRight;
+
+            if(facingRight == true)
+            {
+
+                CheckForPlayer(true);
+
+                if(check.collider != null)
+                {
+                    Debug.Log("Tag of hit: " + check.collider.gameObject.tag);  
+                    DistFromPlayer();
+                    if (check.collider.gameObject.tag == "Player")
+                    {
+                        //Debug.Log("Found player here");
+                        controller.Move(((1 * moveSpeed) * Time.fixedDeltaTime), false, false, false);
+                        chasingPlayer = true;
+                        if (distance <= 5f)
+                        {
+                            if (!attackCooldown)
+                            {
+                                StartCoroutine(Attack(check,true));
+
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (gameObject.transform.position.x >= target2.transform.position.x)
+                        {
+                            goingRightPath = !goingRightPath;
+                            controller.Move(((-1 * moveSpeed) * Time.fixedDeltaTime), false, false, false);
+                            facingRight = !facingRight;
+                        }
+                        else
+                        {
+                            
+                            //Debug.Log("got to the move func");
+                            controller.Move(((1 * moveSpeed) * Time.fixedDeltaTime), false, false, false);
+                        }
+                    }
+                }
+                else if (goingRightPath)
+                {
+                    chasingPlayer = false;
+                    
+                    if (gameObject.transform.position.x >= target2.transform.position.x)
+                    {
+                        goingRightPath = !goingRightPath;
+                        controller.Move(((-1 * moveSpeed) * Time.fixedDeltaTime), false, false, false);
+                        facingRight = !facingRight;
+                    }
+                    else
+                    {
+                        
+                        controller.Move(((1 * moveSpeed) * Time.fixedDeltaTime), false, false, false);
+                    }
+                }
+
+
+            }
+            else
+            {
+
+                CheckForPlayer(true);
+                
+                if (check.collider != null)
+                {
+                    Debug.Log("Tag of hit: " + check.collider.gameObject.tag);
+                    DistFromPlayer();
+                    if (check.collider.gameObject.tag == "Player")
+                    {
+                        //Debug.Log("Found player here");
+                        controller.Move(((-1 * moveSpeed )* Time.fixedDeltaTime), false, false, false);
+                        chasingPlayer = true;
+                        if (distance <= 5f)
+                        {
+                            if (!attackCooldown)
+                            {
+                                StartCoroutine(Attack(check,true));
+
+                            }
+                        }
+                    }
+
+                    else
+                    {
+                        chasingPlayer = false;
+                        if (gameObject.transform.position.x <= target1.transform.position.x)
+                        {
+                            goingRightPath = !goingRightPath;
+                            controller.Move(((1 * moveSpeed) * Time.fixedDeltaTime), false, false, false);
+                            facingRight = !facingRight;
+                        }
+                        else
+                        {
+                            controller.Move(((-1 * moveSpeed) * Time.fixedDeltaTime), false, false, false);
+                        }
+                    }
+
+                }
+                else if (!goingRightPath)
+                {
+                    
+                    if (gameObject.transform.position.x >= target2.transform.position.x)
+                    {
+                        goingRightPath = !goingRightPath;
+                        controller.Move(((1 * moveSpeed) * Time.fixedDeltaTime), false, false, false);
+                        facingRight = !facingRight;
+                    }
+                    else
+                    {
+                        controller.Move(((-1 * moveSpeed) * Time.fixedDeltaTime), false, false, false);
+                    }
+                }
+            }
+        }
+        else
+        {
+            animatorZombie.SetBool("Walking", false);
+        }
+    }
+
+    public void Stunned(float newStunTime)
+    {
+        if(isStunned == true)
+        {
+            Debug.Log("can't stun multiple times");
+        }
+        else
+        {
+            Debug.Log("Enemy Hit");
+            isStunned = true;
+            stunTime = newStunTime;
+            StartCoroutine(StunnedWait());
+        }
+    }
+
+    public void CheckForPlayer(bool isRight)
+    {
+        if (isRight)
+        {
+            check = Physics2D.Raycast(childEyesRight.transform.position, Vector2.right, 50f, layerMask);
+            Debug.DrawRay(childEyesRight.transform.position, -Vector2.right, Color.green, 5f, false);
+        }
+        else
+        {
+            check = Physics2D.Raycast(childEyesRight.transform.position, -Vector2.right, 50f, layerMask);
+            Debug.DrawRay(childEyesRight.transform.position, Vector2.right, Color.green, 5f, false);
+        }
+    }
+
+    public void DistFromPlayer()
+    {
+       
+        distance = Mathf.Abs(check.point.x - transform.position.x);
+        //Debug.Log(distance.ToString() + " " + "Hit: " + check.collider.gameObject.tag + " HitName: " + check.collider.gameObject.name);
+
+    }
+
+    IEnumerator Attack(RaycastHit2D input,bool right)
+    {
+        
+        animatorZ   ombie.SetBool("Attack", true);
+        yield return new WaitForSeconds(1);
+        GetComponent<AudioSource>().Play();
+        if (right)
+        {
+            input.collider.gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(10, 0), ForceMode2D.Impulse);
+
+        }
+        else
+        {
+            input.collider.gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(-10, 0), ForceMode2D.Impulse);
+
+        }
+
+        Debug.Log("Success at attack");
+        attackCooldown = true;
+        StartCoroutine(AttackCooldown());
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        animatorZombie.SetBool("Attack", false);
+        yield return new WaitForSeconds(5);
+        attackCooldown = false;
+    }
+    IEnumerator StunnedWait()
+    {
+
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        gameObject.GetComponent<Collider2D>().enabled = false;
+        yield return new WaitForSeconds(5);
+        Debug.Log("Success at stunn");
+        isStunned = false;
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+        gameObject.GetComponent<Collider2D>().enabled = true;
+        goingRightPath = true;
+    }
+}
--- Assets/ScriptsInternal/PlayerMovement.cs
+++ Assets/ScriptsInternal/PlayerMovement.cs
@@ -2,26 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
     public GameObject cameraFlash;
     public GameObject interactColliderBox;
-    
+    public GameObject lightCamera;
+    public Animator animatorPlayer;
+
+    public bool lightFade = true;
+    public float currentLightIntensity;
     float runSpeed = 40f;
     float horizontalMove = 0f;
     bool isJumping = false;
     public bool isCrouching = false;
     bool isRunning = false;
-     
+    bool isTalking = false;
     Collider2D[] results = new Collider2D[5];
     ContactFilter2D baseFilter;
 
     public float stunPower = 5f;
-
+    public bool flashed;
 
     public int playerCameraAmmo = 12;
+
+    public bool CameraUsed = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,12 +41,23 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         //controller.Move();
+        if (!CameraUsed && !isTalking)
+        {
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+            if(horizontalMove != 0)
+            {
+                animatorPlayer.SetBool("Walking", true);
+            }
+            else
+            {
+                animatorPlayer.SetBool("Walking", false);
+            }
 
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        }
 
         if (Input.GetButtonDown("Jump"))
         {
-            isJumping = true;
+            //isJumping = true;
         }
 
         if (Input.GetButtonDown("Crouch"))
@@ -53,27 +71,35 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("CameraClick"))
         {
-            cameraFlash.SetActive(true);
-            cameraFlash.GetComponent<Collider2D>().OverlapCollider(baseFilter, results);
-
-            for(int i = 0; i < results.Length; i++)
-            {
-                if(results.GetValue(i) != null)
+            if (!CameraUsed) {
+                CameraUsed = true;
+                animatorPlayer.SetBool("Shooting", true);
+                cameraFlash.SetActive(true);
+                cameraFlash.GetComponent<Collider2D>().OverlapCollider(baseFilter, results);
+                //lightCamera.SetActive(true);
+                //animatorPlayer.GetBool("Flashed");
+                for(int i = 0; i < results.Length; i++)
                 {
-                    if(results[i].tag == "Enemy")
+                    if(results.GetValue(i) != null)
                     {
-                        Debug.Log(results[i].name);
-                        results[i].gameObject.GetComponent<EnemyScript>().Stunned(stunPower);
+                        if(results[i].tag == "Enemy")
+                        {
+                            Debug.Log(results[i].name);
+                            results[i].gameObject.GetComponent<EnemyScript>().Stunned(stunPower);
+
+                        }
+
                     }
-
                 }
+                cameraFlash.SetActive(false);
+                //StartCoroutine(CameraFlashWait());
+                lightFade = true;
             }
-            cameraFlash.SetActive(false);
-
         }
 
         if (Input.GetButtonDown("Interact"))
         {
+            isTalking = true;
             interactColliderBox.SetActive(true);
             interactColliderBox.GetComponent<Collider2D>().OverlapCollider(baseFilter, results);
 
@@ -86,12 +112,12 @@ public class PlayerMovement : MonoBehaviour
                         Debug.Log(results[i].name);
                         //results[i].gameObject.GetComponent<EnemyScript>().Stunned(stunPower);
                         results[i].gameObject.GetComponent<InteractAbleScript>().InteractObjectActivate();
-
+                        
                     }
 
                 }
             }
-
+            StartCoroutine(TalkingWait());
             interactColliderBox.SetActive(false);
         }
 
@@ -110,6 +136,65 @@ public class PlayerMovement : MonoBehaviour
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, isCrouching, isJumping, isRunning);
         isJumping = false;
+
+        if (flashed)
+        {
+            lightCamera.SetActive(true);
+        }
+
+        if(lightFade == true)
+        {
+            //Debug.Log("Got to light fade function");
+            if (lightCamera.GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>().intensity >= 0)
+            {
+                Debug.Log(lightCamera.GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>().intensity);
+                //Debug.Log("Got here in fixed update");
+                lightCamera.GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>().intensity -= 2.5f * Time.deltaTime;
+                currentLightIntensity = lightCamera.GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>().intensity;
+            }
+            else
+            {
+                lightCamera.SetActive(false);
+                lightCamera.GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>().intensity = 5;
+                currentLightIntensity = lightCamera.GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>().intensity;
+                lightFade = false;
+                flashed = false;
+            }
+
+        }
+    }
+
+    IEnumerator CameraFlashWait()
+    {
+        while (true)
+        {
+            while(lightCamera.GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>().intensity >= 0)
+            {
+                Debug.Log("Got here in coroutine");
+                lightCamera.GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>().intensity -= 0.000000001f * Time.deltaTime;
+                currentLightIntensity = lightCamera.GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>().intensity;
+            }
+            yield return null;
+            lightCamera.SetActive(false);
+        }
+        //yield return new WaitForSeconds(1);
         
+    }
+
+    IEnumerator TalkingWait()
+    {
+        yield return new WaitForSeconds(1);
+        isTalking = false;
+        animatorPlayer.SetBool("Talking", false);
+    }
+
+    public void CameraHasFlashed()
+    {
+        GetComponent<AudioSource>().Play();
+        Debug.Log("Got flashed again");
+        flashed = true;
+        animatorPlayer.SetBool("Shooting", false);
+        CameraUsed = false;
+        lightFade = true;
     }
 }
